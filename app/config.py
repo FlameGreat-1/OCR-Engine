2e9797ae9640197b8b73fad2db9dd12b259c00a7
FlameGreat-1 <eugochukwu77@gmail.com>
2025-03-17 19:46:32 +0100

    update

--- app/config.py
+++ app/config.py
@@ -28,6 +28,7 @@ class Settings(BaseSettings):
 
     # Google Cloud Vision Configuration
     GCV_CREDENTIALS: str = Field(..., env="GOOGLE_APPLICATION_CREDENTIALS")
+    DOCAI_PROCESSOR_NAME: str = Field(..., env="DOCAI_PROCESSOR_NAME")
 
     # invoice2data Configuration
     INVOICE2DATA_TEMPLATES_DIR: str = Field(default="/app/invoice_templates", env="INVOICE2DATA_TEMPLATES_DIR")
